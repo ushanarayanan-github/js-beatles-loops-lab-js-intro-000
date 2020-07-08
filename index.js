a80a447bbea0b0babe513b1717b34a98ec0e1e20
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-08 03:03:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var emptyArray = new []
+  for (i=0, i<musicians.length, i++)
+  {
+    emptyArray.push=musicians[i] + " plays " + instruments[0]
+  }
+  return emptyArray
+}// add solution here
