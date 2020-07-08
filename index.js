45a558221f52f341e53db6357027886870034eaf
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-08 03:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 function theBeatlesPlay(musicians,instruments) {
-  var emptyArray = new []
-  for (i=0, i<musicians.length, i++)
+  const emptyArray = new Array()
+  for (let i=0; i<musicians.length; i++)
   {
-    emptyArray.push=musicians[i] + " plays " + instruments[0]
+    emptyArray.push(musicians[i] + " plays " + instruments[i])
   }
   return emptyArray
 }// add solution here
+
+function johnLennonFacts(facts) {
+  const emptyArray = new Array()
+  let num i=0
+  do
+  {
+    emptyfacts.push(facts[i]+"!!!")  
+    i++
+  }
+  while (i <= facts.length)
+  return emptyArray
+}
